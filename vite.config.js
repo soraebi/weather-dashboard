e3cd4cfd2bc7e8bbe9c071baecb9106b41831d70
空野あまえび <sorano.amaebi@gmail.com>
2025-06-15 16:55:10 +0900

    fix : vue router base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/weather-dashboard/',
   plugins: [vue()],
   define: {
     __VUE_OPTIONS_API__: true,
